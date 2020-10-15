a2b50ff5b0a41b1e0cbf8b2d6f50551f51b281d6
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2020-10-15 08:28:25 -0400

    Enable more future options for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   future: {
     removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true,
   },
   experimental: {
     applyComplexClasses: true,
